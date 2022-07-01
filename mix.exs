de5ac40167412a38608d11f90e91c3d27640fd1e
admin <admin@gmail.com>
2022-07-01 12:06:27 +0800

    Match phoenix version to 1.5.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxDigest.MixProject do
   def project do
     [
       app: :phx_digest,
-      version: "0.1.0",
+      version: "1.5.6",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
